44c4838902983cbb278be6bb3cb47780d2d88b28
Josh Walters <josh.walters@outlook.com>
2019-01-14 21:43:09 -0600

    removed unused babel dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,10 @@
 const gulp = require('gulp');
-// const babel = require('gulp-babel');
 const del = require('del');
 const ts = require('gulp-typescript');
 
 const files = ['src/**/*.ts', '!src/**/*.spec.ts'];
 
 gulp.task('default', ['clean'], () => {
-  // return gulp
-  //   .src(files)
-  //   .pipe(babel())
-  //   .pipe(gulp.dest('lib'));
   const tsProject = ts.createProject('tsconfig.json');
   return gulp
     .src(files)
