c18a3918d89da973834994af178eeec1e5806fbf
Josh Walters <josh.walters@outlook.com>
2019-01-16 16:43:46 -0600

    fixed exports of cached-item types

--- src/cached-item/index.ts
+++ src/cached-item/index.ts
@@ -1,5 +1,3 @@
-import { TimeoutCachedStorageItem } from './timeout-cached-storage-item';
-import { ApiCachedResponseHelper } from './api-cached-response-helper';
-import { TimeoutCachedItem } from './timeout-cached-item';
-
-export { TimeoutCachedItem, TimeoutCachedStorageItem, ApiCachedResponseHelper };
+export * from './timeout-cached-storage-item';
+export * from './api-cached-response-helper';
+export * from './timeout-cached-item';
