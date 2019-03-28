3bb71961b0920e3264e6c18a6c2c4b8c8f030e39
Josh Walters <josh.walters@outlook.com>
2019-03-28 16:50:38 -0500

    fixed linting issues

--- src/logging/abstract-logger.ts
+++ src/logging/abstract-logger.ts
@@ -4,23 +4,23 @@ import { LogLevel } from './log-level';
 export abstract class AbstractLogger implements Logger {
   constructor(protected level = LogLevel.None) {}
 
-  debug(message: any, ...args: any[]): void {
+  public debug(message: any, ...args: any[]): void {
     this.logAtLevel(LogLevel.Debug, message, ...args);
   }
 
-  info(message: any, ...args: any[]): void {
+  public info(message: any, ...args: any[]): void {
     this.logAtLevel(LogLevel.Info, message, ...args);
   }
 
-  warn(message: any, ...args: any[]): void {
+  public warn(message: any, ...args: any[]): void {
     this.logAtLevel(LogLevel.Warn, message, ...args);
   }
 
-  error(message: any, ...args: any[]): void {
+  public error(message: any, ...args: any[]): void {
     this.logAtLevel(LogLevel.Error, message, ...args);
   }
 
-  canLog(level: LogLevel): boolean {
+  public canLog(level: LogLevel): boolean {
     return this.level <= level;
   }
 
